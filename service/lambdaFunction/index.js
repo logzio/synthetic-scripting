492f5e5978296a7963612e5c3d2852e2c75a8170
AntonKolomiiets <anton.kolomiiets@logz.io>
2023-02-15 11:34:48 +0200

    add optionfor first invocation

--- service/lambdaFunction/index.js
+++ service/lambdaFunction/index.js
@@ -1,64 +1,55 @@
 const playwright = require('playwright-aws-lambda');
-	const path = require('path');
-	const readSendData = require('./rsData');
-	const cfnResponse = require('cfn-response-async');
-	
-	const firstRun = async (event, context) => {
-		await regularRun();
-	
-		return await cfnResponse.send(
-			event,
-			context,
-			'SUCCESS',
-			{},
-			'first-invoke-id',
-		);
-	};
-	
-	const regularRun = async () => {
-		let context = null;
-		let err = null;
-		let page = null;
-		try {
-			browser = await playwright.launchChromium(false);
-			context = await browser.newContext({
-				recordHar: {
-					path: path.join(__dirname, '..', '..', 'tmp', 'example.har'),
-					mode: 'full',
-					content: 'omit',
-				},
-			});
-			await context.tracing.start({ screenshots: false, snapshots: false });
-	
-			page = await context.newPage();
-	
+const path = require('path');
+const readSendData = require('./rsData');
+const cfnResponse = require('cfn-response-async');
 
-							await page.goto('https://app.logz.io/');
-await page.getByPlaceholder('you@yourcompany.com').fill('doronbargo+10@gmail.com');
-await page.getByPlaceholder('****************').fill('password');					
-		
+const firstRun = async (event, context) => {
+    await regularRun();
 
+    return await cfnResponse.send(
+        event,
+        context,
+        'SUCCESS',
+        {},
+        'first-invoke-id',
+    );
+};
 
+const regularRun = async () => {
+    let context = null;
+    let err = null;
+    let page = null;
+    try {
+        browser = await playwright.launchChromium(false);
+        context = await browser.newContext({
+            recordHar: {
+                path: path.join(__dirname, '..', '..', 'tmp', 'example.har'),
+                mode: 'full',
+                content: 'omit',
+            },
+        });
+        await context.tracing.start({ screenshots: false, snapshots: false });
 
-} catch (error) {
-	err = error.message;
-} finally {
-	if (browser) {
-		await context.tracing.stop({
-			path: path.join(__dirname, '..', '..', 'tmp', 'trace.zip'),
-		});
-		await context.close();
-		await browser.close();
-	}
-}
-await readSendData(err);
-return true;
+        page = await context.newPage();
+    } catch (error) {
+        err = error.message;
+    } finally {
+        if (browser) {
+            await context.tracing.stop({
+                path: path.join(__dirname, '..', '..', 'tmp', 'trace.zip'),
+            });
+            await context.close();
+            await browser.close();
+        }
+    }
+    await readSendData(err);
+    return true;
 };
 
 exports.handler = async (event, context) => {
-if (event.RequestType === 'Create') {
-	return await firstRun(event, context);
-} else {
-	return await regularRun();
-}
+    if (event.RequestType === 'Create' || event.RequestType === 'Delete') {
+        return await firstRun(event, context);
+    } else {
+        return await regularRun();
+    }
 };
