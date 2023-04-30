12d00174971b2eddb17d72f26f9e323da68485b4
Anton Kolomiiets <anton.kolomiiets@logz.io>
2023-04-30 11:39:24 +0300

    update docker size with proper dependencies (#17)

--- Dockerfile
+++ Dockerfile
@@ -1,20 +1,27 @@
-# Notice: To save space, npm install was run on local machine
 
-#Specify a base image
-FROM mcr.microsoft.com/playwright:v1.32.0
+FROM mcr.microsoft.com/playwright:v1.32.3
+
+ARG PLAYWRIGHT_SKIP_BROWSER_DOWNLOAD=1
 
 WORKDIR /usr/app
 
 #Install dependecies
 
 COPY ./package.json ./
-
+RUN npm install
 COPY ./ ./
+
+WORKDIR /usr/app/service/lambdaFunction
+
+RUN npm install
 
 WORKDIR /usr/app/client
 
+RUN npm install
 
 RUN npm run build
+
+RUN rm -rf node_modules/
 
 WORKDIR /usr/app
 
--- utils/lambdaFunctionLocal/index.js
+++ utils/lambdaFunctionLocal/index.js
@@ -1,36 +1,17 @@
 const playwright = require('playwright-aws-lambda');
-	const { chromium } = require('playwright-core');
-	const path = require('path');
 
-	const errorStatusHandler = require('./statusError');
-	
-	const handlerLocally = async () => {
-		let context = null;
-		let err = null;
-		let page = null;
-		let browser = null;
-		try {
-			
-			browser = await chromium.launch({});
-			context = await browser.newContext();
-			page = await context.newPage();
-	
+const errorStatusHandler = require('./statusError');
 
-		
-await page.goto('https://logz.io/');
-
-
-await page.waitForSelector('.site-footer__menu_inner_col-2 > .site-footer__menu_group:nth-child(1) > .site-footer__menu_group_list > .site-footer__menu_group_item:nth-child(2) > a')
-await page.click('.site-footer__menu_inner_col-2 > .site-footer__menu_group:nth-child(1) > .site-footer__menu_group_list > .site-footer__menu_group_item:nth-child(2) > a')
-
-await page.waitForURL('**/careers/');
-
-await page.waitForSelector('.col-lg-12 > .colomn-footer:nth-child(4) > .footer_list > li:nth-child(2) > a')
-await page.click('.col-lg-12 > .colomn-footer:nth-child(4) > .footer_list > li:nth-child(2) > a')
-
-await page.waitForURL('**/blog/');				
-		
-       } catch (error) {
+const handlerLocally = async () => {
+    let context = null;
+    let err = null;
+    let page = null;
+    let browser = null;
+    try {
+        browser = await playwright.launchChromium();
+        context = await browser.newContext();
+        page = await context.newPage();
+    } catch (error) {
         err = error.message;
     } finally {
         if (browser) {
@@ -40,7 +21,6 @@ await page.waitForURL('**/blog/');
     }
     let status = errorStatusHandler(err);
     return status;
-
 };
-handlerLocally()
+handlerLocally();
 // module.exports = handlerLocally;
